3d22b065d74d6f11803df0adfd70f46d2478f4f5
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-16 21:28:12 +0100

    comment schema done

---
+++ models/Comment.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const CommentSchema = new mongoose.Schema({
+  comment: { type: String, required: true,},
+  likes: { type: Number, required: true,},
+  recipe: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Recipe",
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
