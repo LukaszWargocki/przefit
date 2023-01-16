fbb78faa584866e9398bfa374b30ccf9752c5829
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-16 21:22:24 +0100

    first iteration of recipe schema

---
+++ models/Recipe.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const RecipeSchema = new mongoose.Schema({
+  recipeName: { type: String, required: true },
+  ingredients: { type: [String], required: true },
+  restrictedFor: [String],
+  tags: [String],
+  instructions: { type: [String], required: true },
+  imageLink: { type: String, required: true },
+  cloudinaryId: { type: String, required: true },
+  prepTime: { type: Number, required: true },
+  cookTime: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  createdOn: { type: Date, default: Date.now },
+  allergies: [String],
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Recipe", RecipeSchema);
