45230501ccea2bcf4db40ffd398ca70cd102e639
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-25 23:00:36 +0100

    ensure auth / ensure guest middleware

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+// middleware checking if/if not authenticated
+// TODO: comments
+module.exports = {
+  ensureAuth: function(req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/");
+    }
+  },
+  ensureGuest: function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/dashboard");
+    }
+  },
+};
