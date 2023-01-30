f7f055f65a619a263e95d7ee1c1899e7ffa37607
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-30 18:13:37 +0100

    renamed "recipe" field to "post" for reference

--- models/Comment.js
+++ models/Comment.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const CommentSchema = new mongoose.Schema({
   comment: { type: String, required: true,},
   likes: { type: Number, required: true,},
-  recipe: { 
+  post: { 
     type: mongoose.Schema.Types.ObjectId,
     ref: "Post",
   },
