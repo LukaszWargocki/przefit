edc76e0929b62e23f9640d0baa42b7307792350e
Lukasz Wargocki <lmwargocki@gmail.com>
2023-01-30 18:11:13 +0100

    comments controller added - adding comment objects to db in relation to specific posts

---
+++ controllers/comments.js
@@ -0,0 +1,19 @@
+const Comment = require("./models/Comment");
+// adding comments to posts
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      // create Comment object in db
+      await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+      });
+      console.log("Comment has been added.");
+      // redirect to commented post page
+      res.redirect("/post/"+req.params.id);
+    } catch(err) {
+      console.log(err);
+    }
+  },
+};
